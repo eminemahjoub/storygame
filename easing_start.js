7b29956cdfa0553cee73c6a40fea7d28a5bbb4d7
Amine Mahjoub <31483078+eminemahjoub@users.noreply.github.com>
2022-10-01 12:43:10 +0200

    Update easing_start.js

--- easing_start.js
+++ easing_start.js
@@ -10,7 +10,7 @@ let startAnimation = () => {
     let startKeyframes = {"left": "2vw"};
     let endKeyframes = {"left": "100vw"};
     let options = {
-        duration: 3000
+        duration: 300
     };
 
     car.animate([startKeyframes, endKeyframes], options);
